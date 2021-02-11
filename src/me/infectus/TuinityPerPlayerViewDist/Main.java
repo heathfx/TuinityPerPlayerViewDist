4f23103cc4fe02878a5a9161bc10e76a9e0489b5
HeathFX <none>
2021-02-11 01:08:27 -0600

    20210211 Add check for absence of vd permission and set viewdistance back to server default

--- src/me/infectus/TuinityPerPlayerViewDist/Main.java
+++ src/me/infectus/TuinityPerPlayerViewDist/Main.java
@@ -1,13 +1,11 @@
 package me.infectus.TuinityPerPlayerViewDist;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.logging.Level;
 import lombok.extern.java.Log;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
@@ -15,42 +13,40 @@ import org.bukkit.entity.Player;
 @Log
 public class Main extends JavaPlugin {
     
+    public HashMap<String, Boolean> playerUsingPermBasedVd = new HashMap<>();
 
     @Override
     public void onEnable() {
-        /*
-        //check if running on Tuinity with support for per-player vd
-        PluginManager pluginManager = getServer().getPluginManager();
-        boolean isTuinity = false;
-        try {
-            if(Arrays.toString(Class.forName("org.bukkit.entity.Player").getInterfaces()).contains("setNoTickViewDistance")) isTuinity = true;
-        } catch (ClassNotFoundException exception) {
-            isTuinity = false;
-        }
-        if (!isTuinity) {
-            log.log(Level.INFO, "{0}This plugin requires Tuinity with per-player view-distance to function; disabling plugin", ChatColor.RED);
-            pluginManager.disablePlugin(this);
-            return;
-        }
-        */
         
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
                 Bukkit.getOnlinePlayers().forEach((player) -> {
                     int vd = getVdPermission(player);
-                    if(vd == -1) return;
-                    if(vd > 32) vd = 32;
                     int currentVd = player.getNoTickViewDistance();
-                    if(vd == currentVd) return;
+                    
+                    if(vd == currentVd) return; //nothing to do for this player.
+                    
+                    String pName = player.getName();
+                    if(vd == -1) {
+                        if(playerUsingPermBasedVd.containsKey(pName) && playerUsingPermBasedVd.get(pName)) {
+                            player.setNoTickViewDistance(vd);
+                            playerUsingPermBasedVd.put(pName, false);
+                            log.log(Level.INFO, "{0}Un-ticked view-distance changed from {1} to the server default for player {2} because no vd.max.<vd> permission node was found.", new Object[]{ChatColor.AQUA, currentVd, pName});
+                        }
+                        return;
+                    }
+                    
+                    if(vd > 32) vd = 32; //cap the max vd to 32 chunks
+                    
                     player.setNoTickViewDistance(vd);
-                    log.log(Level.INFO, "{0}Un-ticked view-distance changed from {1} to {2} for player {3} via permissions.", new Object[]{ChatColor.AQUA, currentVd, vd, player.getName()});
-
+                    playerUsingPermBasedVd.put(pName, true);
+                    log.log(Level.INFO, "{0}Un-ticked view-distance changed from {1} to {2} for player {3} via permissions.", new Object[]{ChatColor.AQUA, currentVd, vd, pName});
                 });
             }
         },
         0L,
-        20L);
+        100L);
     }
     
     public int getVdPermission(Player player) {
